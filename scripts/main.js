3ad765bc90df6fd198b3a72a383a61326417494c
tessema2021 <ninafuchere@gmail.com>
2021-09-07 09:55:51 -0500

    function added to fetch randomUser

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-console.log("You look marvelous!")
+//console.log("You look marvelous!")
+
+const getRondamUser = () => {
+    return fetch("https://randomuser.me/")
+        .then(response => response.json())
+        .then(data =>
+            display.innerHTML = data.randomUser
+        )
+
+
+}
+getRondamUser();
